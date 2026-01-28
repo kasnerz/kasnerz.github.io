da6c8f345cba7dcbb4dff43ff1915e61342d3925
Zdenek Kasner <zdenek.kasner@gmail.com>
2026-01-28 10:18:57 +0100

    Init

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://kasnerz.github.io/", // replace this with your deployed domain
+  author: "Zdeněk Kasner",
+  profile: "https://kasnerz.github.io/",
+  desc: "Personal website of Zdeněk Kasner, NLP researcher.",
+  title: "Zdeněk Kasner",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
